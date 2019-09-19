e58fedb538820f150db7d9a6347b32077f9c7b76
j-woodlee <jake.woodlee@ricardodefense.com>
2019-09-19 11:45:53 -0700

    added Delete update and create functions

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -17,53 +17,53 @@ namespace StudentsApi.Controllers {
         public ActionResult<List<Student>> Get() =>
             this._studentService.Get();
 
-        // [HttpGet("{id:length(24)}", Name = "GetStudent")]
-        // public ActionResult<Student> Get(string id) {
-        //     var student = this._studentService.Get(id);
+        [HttpGet("{id:length(24)}", Name = "GetStudent")]
+        public ActionResult<Student> Get(string id) {
+            var student = this._studentService.Get(id);
 
-        //     if (student == null) {
-        //         return NotFound();
-        //     }
+            if (student == null) {
+                return NotFound();
+            }
 
-        //     return student;
-        // }
+            return student;
+        }
 
-        // [HttpPost]
-        // public ActionResult<Student> Create(Student student)
-        // {
-        //     _studentService.Create(student);
+        [HttpPost]
+        public ActionResult<Student> Create(Student student)
+        {
+            _studentService.Create(student);
 
-        //     return CreatedAtRoute("GetStudent", new { id = student.Id.ToString() }, student);
-        // }
+            return CreatedAtRoute("GetStudent", new { id = student.Id.ToString() }, student);
+        }
 
-        // [HttpPut("{id:length(24)}")]
-        // public IActionResult Update(string id, Student studentIn)
-        // {
-        //     var student = _studentService.Get(id);
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Student studentIn)
+        {
+            var student = _studentService.Get(id);
 
-        //     if (student == null)
-        //     {
-        //         return NotFound();
-        //     }
+            if (student == null)
+            {
+                return NotFound();
+            }
 
-        //     _studentService.Update(id, studentIn);
+            _studentService.Update(id, studentIn);
 
-        //     return NoContent();
-        // }
+            return NoContent();
+        }
 
-        // [HttpDelete("{id:length(24)}")]
-        // public IActionResult Delete(string id)
-        // {
-        //     var student = _studentService.Get(id);
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var student = _studentService.Get(id);
 
-        //     if (student == null)
-        //     {
-        //         return NotFound();
-        //     }
+            if (student == null)
+            {
+                return NotFound();
+            }
 
-        //     _studentService.Remove(student.Id);
+            _studentService.Remove(student.Id);
 
-        //     return NoContent();
-        // }
+            return NoContent();
+        }
     }
 }
--- Models/Student.cs
+++ Models/Student.cs
@@ -5,6 +5,8 @@ namespace StudentsApi.Models {
 
     public class Student {
 
+        // Each property is mapped to a key
+
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
@@ -13,14 +15,51 @@ namespace StudentsApi.Models {
         public string StudentName { get; set; }
 
         [BsonElement("SAT")]
-        public string SATScores { get; set; }
+        public SATScores SATScores { get; set; }
 
-        [BsonElement("TestObject")]
-        public TestObjectInstance TestObject { get; set; }
+        [BsonElement("Absences")]
+        public string[] Absences { get; set; }
+
+        [BsonElement("Living Situation")]
+        public string LivingSituation { get; set; }
+
+        [BsonElement("Co-curriculars")]
+        public string[] CoCurriculars { get; set; }
+
+        [BsonElement("Grades")]
+        public Grades Grades { get; set; }
+
     }
 
-    public class TestObjectInstance {
-        [BsonElement("key")]
-        public string key { get; set; }
+    public class SATScores {
+
+        [BsonElement("latest")]
+        public string latest { get; set; }
+
+        [BsonElement("2019")]
+        public string TwoThousand19 { get; set; }
+
+        [BsonElement("2018")]
+        public string TwoThousand18 { get; set; }
+    }
+
+    public class Grades {
+
+        [BsonElement("2018")]
+        public Courses TwoThousand18 { get; set; }
+
+        [BsonElement("2019")]
+        public Courses TwoThousand19 { get; set; }
+
+    }
+
+    public class Courses {
+
+        public string Math { get; set; }
+
+        public string Science { get; set; }
+
+        public string English { get; set; }
+        
     }
 }
